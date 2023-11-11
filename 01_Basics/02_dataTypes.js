28df6c8f77e7841c059eb19d1893246ad352b902
tahirhussain0909 <tahirhussain5112@gmail.com>
2023-11-11 15:59:24 +0530

    Currently learning the DataTypes in JS

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3); // we are using nodejs, not browser
+
+console.log(
+    3 + 3
+); /** this is not good code readability and code readability should be high */
+
+console.log("Tahir");
+
+let name = "Tahir";
+
+let age = 18;
+
+let isLoggedIn = false;
+
+let state = null;
+
+let myName;
+
+// number => 2 to power 52 is the range
+// BigInt
+// string => ""
+// boolean => true/false
+// null => standalone value and is a type
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "Tahir");
+console.log(typeof age);
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object and also this is asked in interviews
