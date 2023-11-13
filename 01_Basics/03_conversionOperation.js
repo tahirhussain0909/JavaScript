f90228e5be50d706806690916c6d81ecc7595bab
tahirhussain0909 <tahirhussain5112@gmail.com>
2023-11-13 10:11:42 +0530

    conversionOperation topic completed

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = "33abc";
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+if (valueInNumber == Number) {
+    console.log(typeof valueInNumber);
+} else {
+    console.log("valueInNumber is not a number");
+}
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "tahir";
+
+let booleanIsLoggedIN = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIN);
+
+// 1 => true; 0 => false
+// "" => false
+// "tahir" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
