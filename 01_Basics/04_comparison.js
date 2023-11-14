7a0b34c34deb9d9544ef06dd3517a63b6ae9bb8e
tahirhussain0909 <tahirhussain5112@gmail.com>
2023-11-14 09:40:12 +0530

    comparison topic is going on

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,17 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(
+    null >= 0
+); /** why the null is >= to zero because the comparison operator
+ >= works differently its not like == or > , < it is very different */
+
+console.log();
