4e38859fbee70477246cc0ee151c873a94c13e09
tahir hussain <tahirhussain5112@gmail.com>
2023-11-14 05:04:28 +0000

    comaprison operator and some dataytypes are completed

--- 01_Basics/04_comparison.js
+++ 01_Basics/04_comparison.js
@@ -9,9 +9,14 @@
 
 console.log(null > 0);
 console.log(null == 0);
-console.log(
-    null >= 0
-); /** why the null is >= to zero because the comparison operator
- >= works differently its not like == or > , < it is very different */
+console.log(null >= 0);
+/** why the null is >= to zero because the comparison operator
+>= works differently its not like == or > , < it is very different */
 
-console.log();
+// console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+// ===
+
+console.log("2" === 2);
