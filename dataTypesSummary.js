c8009938053d52f6c5d65e3048276c38dd26366d
tahir hussain <tahirhussain5112@gmail.com>
2023-11-15 07:37:21 +0000

    i dont know

---
+++ dataTypesSummary.js
@@ -0,0 +1,21 @@
+// # Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail = undefined;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+console.log(id === anotherId);
+
+const bigNumber = 12343434323255351232n;
+
+// # Reference (Non primitive)
+
+// Array, Objects, Functions
