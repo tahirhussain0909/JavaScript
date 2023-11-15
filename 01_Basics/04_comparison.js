b471610253d8334ac6c6a3bc6c56e9db7665a060
tahirhussain0909 <tahirhussain5112@gmail.com>
2023-11-15 13:49:42 +0530

    done

--- 01_Basics/04_comparison.js
+++ 01_Basics/04_comparison.js
@@ -13,10 +13,10 @@ console.log(null >= 0);
 /** why the null is >= to zero because the comparison operator
 >= works differently its not like == or > , < it is very different */
 
-// console.log(undefined > 0);
+console.log(undefined > 0);
 console.log(undefined == 0);
 console.log(undefined >= 0);
 
 // ===
 
-console.log();
+console.log("2" === 2);
